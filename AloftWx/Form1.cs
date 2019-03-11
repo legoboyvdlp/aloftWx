acd842f47e5260dc336a2a4b2acca9e2037b502a
legoboyvdlp R <legoboyvdlp@gmail.com>
2019-03-11 16:18:30 +0000

    Add button which when clicked will download the .grib2 files

--- AloftWx/Form1.cs
+++ AloftWx/Form1.cs
@@ -6,16 +6,19 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 // This is the code for your desktop app.
 // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
 
 namespace AloftWx
 {
-    public partial class Form1 : Form
+    public partial class AloftWxForm : Form
     {
-        public Form1()
+        public AloftWxForm()
         {
             InitializeComponent();
         }
@@ -27,9 +30,49 @@ namespace AloftWx
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Thanks!");
+            string cycleStr = Program.GetDateCycle();
+            int curCycle = int.Parse(cycleStr.Substring(Math.Max(0, cycleStr.Length - 2)));
+            string forecast = Program.GetForecast(curCycle);
+            string filename = Program.GetFilename(cycleStr, forecast);
+            
+            // HTTP request
+
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    string NOAAurl = "http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs." + cycleStr + "/" + filename;
+                    Console.WriteLine(NOAAurl);
+                    HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create
+                        (string.Format(NOAAurl));
+
+                    WebReq.Method = "GET";
+
+                    HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
+                    if (WebResp.StatusCode != HttpStatusCode.OK)
+                    {
+                        Console.WriteLine("Server status: " + WebResp.StatusCode);
+                    }
+                    Stream Answer = WebResp.GetResponseStream();
+                    Answer.CopyTo(myStream);
+                    
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
         }
     }
 }
--- AloftWx/Program.cs
+++ AloftWx/Program.cs
@@ -8,15 +8,98 @@ namespace AloftWx
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AloftWxForm());
+        }
+
+        public static string GetDateCycle()
+        {
+            // declare variables
+            long currentYear = long.Parse(DateTime.UtcNow.Year.ToString());
+            long currentMonth = long.Parse(DateTime.UtcNow.Month.ToString());
+            long currentDay = long.Parse(DateTime.UtcNow.Day.ToString());
+            long currentHour = long.Parse(DateTime.UtcNow.Hour.ToString());
+            long curCycle = 0;
+
+            // declare arrays
+            int[] cycles = new int[] { 0, 6, 12, 18 };
+
+            // adjust hour by removing 5
+            // a cycle is published between 4 and 5 hours after the cycle time. 
+            // cycle gfs.2018121806 was published at 10:54 UTC (4 hours 54 min)
+            currentHour = currentHour - 5;
+
+            // if the adjusted cycle is less than 0, add 24 hours and reduce day by 1
+            if (currentHour < 0)
+            {
+                currentHour += 24;
+                currentDay -= 1;
+            }
+
+            // get the correct cycle
+            for (var i = 3; i > 0; i--)
+            {
+                if (currentHour >= cycles[i])
+                {
+                    curCycle = cycles[i];
+                    break;
+                }
+            }
+
+            // if the cycle is 0, add an extra 0
+            string curCycleStr = curCycle.ToString();
+            curCycleStr = 0 + curCycleStr;
+
+            // if the month is less than 10, add an extra 0
+            string currentMonthStr = currentMonth.ToString();
+            if (currentMonth < 10) { currentMonthStr = 0 + currentMonth.ToString(); }
+
+            return currentYear.ToString() + currentMonthStr + currentDay.ToString() + curCycleStr;
+        }
+
+        public static string GetForecast(int cycle)
+        {
+            // declare variable
+            long currentHour = long.Parse(DateTime.UtcNow.Hour.ToString());
+            long currentDay = long.Parse(DateTime.UtcNow.Day.ToString());
+            int forecast = 0;
+
+            // declare array
+            int[] forecasts = new int[] { 6, 9, 12, 15, 18, 21, 24 };
+
+            // adjust hour by removing 5
+            currentHour = currentHour - 5;
+
+            // if the adjusted hour is less than 0, add 24 hours and reduce day by 1
+            if (currentHour < 0)
+            {
+                currentHour += 24;
+                currentDay -= 1;
+            }
+
+            // get the correct forecast
+            for (var i = 6; i > 0; i--)
+            {
+                if (currentHour >= forecasts[i])
+                {
+                    forecast = forecasts[i];
+                    break;
+                }
+            }
+
+            string forecastStr = forecast.ToString();
+            if (forecast < 10) { forecastStr = 0 + forecastStr; }
+
+            return forecastStr;
+        }
+        public static string GetFilename(string cycleStr, string forecast)
+        {
+            string filename = "WAFS_blended_"  + cycleStr + "f" + forecast + ".grib2";
+            return filename;
         }
     }
 }
